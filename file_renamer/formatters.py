13e18da61684554312c65eecbf338441f7e0b3b0
ali-charara <ali.charar@hotmail.fr>
2022-11-09 02:26:33 +0100

    fix: changed regex of snake case formatter to raw

--- file_renamer/formatters.py
+++ file_renamer/formatters.py
@@ -5,5 +5,5 @@ def format_to_snake_case(string: str) -> str:
     return re.sub(
         r"[\s_\-—]+",
         "_",
-        re.sub("(?<=[a-z0-9])([A-Z]+)|(?<=[A-Z]{2})([a-z]+)", r" \1", string.strip()),
+        re.sub(r"(?<=[a-z0-9])([A-Z]+)|(?<=[A-Z]{2})([a-z]+)", r" \1", string.strip()),
     ).lower()
