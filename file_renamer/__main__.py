4dfbeab59bd2a1892c45d430fb81d2048bfe84c3
ali-charara <ali.charar@hotmail.fr>
2022-11-09 02:30:52 +0100

    feat: --clean cleaner added to remove unwanted patterns in names due to previous operations as copies, exchange name formatting (e.g. [1234])

--- file_renamer/__main__.py
+++ file_renamer/__main__.py
@@ -1,12 +1,18 @@
 from argparse import ArgumentParser, Namespace
 import os
 import logging
+from functools import partial
 
 from unidecode import unidecode
 
-from .constants import WHITELIST_EXTENSIONS, FORMATTER, LOGGER_VERBOSE
+from .constants import (
+    WHITELIST_EXTENSIONS,
+    FORMATTER,
+    LOGGER_VERBOSE,
+    REGEX_UNWANTED_PATTERNS,
+)
 from .path_processing import search_files_and_directories, rename_file
-from .utils import input_yes_no_answer
+from .utils import input_yes_no_answer, remove_unwanted_patterns
 
 logging.basicConfig()
 logger = logging.getLogger("file renamer")
@@ -30,6 +36,13 @@ def parse_arguments() -> Namespace:
         default=False,
         help="whether to cast file_names to ascii format (default: False)",
     )
+    parser.add_argument(
+        "--clean",
+        "-c",
+        action="store_true",
+        default=False,
+        help="whether to remove unwanted patterns if possible (e.g. '- copy') (default: False)",
+    )
 
     return parser.parse_args()
 
@@ -40,17 +53,39 @@ if __name__ == "__main__":
     if input_yes_no_answer(
         f"Do you want to process {(absolute_folder_path := os.path.abspath(arguments.folder_path))}?"
     ):
+        preprocesser = unidecode if arguments.ascii else lambda x: x
+        cleaner = (
+            partial(
+                remove_unwanted_patterns,
+                regex_unwanted_patterns=REGEX_UNWANTED_PATTERNS,
+            )
+            if arguments.clean
+            else lambda x: x
+        )
+
         logger.info("Processing files...")
         # ? relative paths may raise errors due to MAX_PATH limitations on windows for instance
         candidates = search_files_and_directories(absolute_folder_path, arguments.deep)
-        pre_processer = unidecode if arguments.ascii else lambda x: x
         for file_path, is_directory in reversed(candidates):
             full_path, dot_extension = os.path.splitext(file_path)
             file_name = full_path.split(os.sep)[-1]
-            if is_directory or dot_extension[1:] in WHITELIST_EXTENSIONS:
-                if file_name != (formatted_name := FORMATTER(pre_processer(file_name))):
-                    rename_file(file_path, f"{formatted_name}{dot_extension}")
-                    logger.info(f"{file_name} → {formatted_name}")
-                    number_formatted_files += 1
+            if (
+                is_directory or dot_extension[1:] in WHITELIST_EXTENSIONS
+            ) and file_name != (
+                formatted_name := FORMATTER(cleaner(preprocesser(file_name)))
+            ):
+                try:
+                    rename_file(
+                        file_path,
+                        f"{(formatted_name)}{dot_extension}",
+                    )
+                except FileExistsError:
+                    rename_file(
+                        file_path,
+                        f"{(formatted_name := FORMATTER(preprocesser(file_name)))}{dot_extension}",
+                    )
+
+                logger.info(f"{file_name} → {formatted_name}")
+                number_formatted_files += 1
 
         print(f"Number of files formatted: {number_formatted_files}")
--- file_renamer/constants.py
+++ file_renamer/constants.py
@@ -12,8 +12,15 @@ WHITELIST_EXTENSIONS: list[str] = [
 PROJECT_INDICATORS: list[str] = [
     ".renamer_ignore", ".git", ".obsidian"
     ]
+
+UNWANTED_PATTERNS: list[str] = [
+    r"[\s_]\(\d\)$",  r"\[\d{4,}\]$", r"\- Copy$",
+    r"[\s_]?copy$",
+]
 # fmt: on
 PRIVATE_HEADERS: list[str] = [".", "_"]
 
+
+REGEX_UNWANTED_PATTERNS = r"|".join(UNWANTED_PATTERNS)
 FORMATTER: Callable = format_to_snake_case
 LOGGER_VERBOSE: LoggingLevel = LoggingLevel.INFO
